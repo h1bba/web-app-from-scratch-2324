404a3e319187f90f7aaec6460004a592ca8c2957
Ufuk <hibbayt@gmail.com>
2024-02-08 12:41:34 +0100

    lokale werkende json api

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,30 @@
-fetch('https://api.quotable.io').then(res => res.json()).then(data => console.log(data))
+const usersNameEl = document.getElementById('naam');
+const userAgeEl = document.getElementById('age');
+const userStadEl = document.getElementById('stad')
+
+// Data fetchen
+fetch("/data/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+      // Data showcasen
+  for (let users of data) {
+    // console.log(users);
+    const userNaam = document.createElement("h2");
+    const userAge = document.createElement('h2')
+    const userStad = document.createElement('h2')
+    userNaam.innerText = users.name
+    userAge.innerText = users.age
+    userStad.innerText = users.stad
+    usersNameEl.append(userNaam)
+    userAgeEl.append(userAge)
+    userStadEl.append(userStad)
+
+
+
+  }
+
+  })
+  .catch((error) => console.error("Error loading JSON file", error));
+  
