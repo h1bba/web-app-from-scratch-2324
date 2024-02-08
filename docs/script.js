7ec45f06b8b56eaa84bf948858e43c5a810d038f
Ufuk <hibbayt@gmail.com>
2024-02-08 14:45:07 +0100

    skills.json added & start of the tools api

--- docs/script.js
+++ docs/script.js
@@ -17,4 +17,34 @@ fetch("/data/data.json")
   }
   })
   .catch((error) => console.error("Error loading JSON file", error));
-  
+
+  // Data fetchen
+fetch("/data/skills.json")
+.then((response) => response.json())
+.then((data) => {
+  console.log(data);
+
+// Data showcasen
+for (let tool of data) {
+    const skillscontainer = document.querySelector(".skillscontainer")
+    console.log(tool);
+    
+//     // nieuwe div aanmaken VOOR elke skill
+    // const skillNameEl = document.createElement('p');
+    // const skillImgEl = document.createElement('img');
+
+
+
+    // skillNameEl.innerText = tool.skill;
+
+//   const usersNameEl = document.getElementById('naam');
+//   const userAgeEl = document.getElementById('age');
+//   const userStadEl = document.getElementById('stad');
+
+//   usersNameEl.innerText = users.name;
+//   userAgeEl.innerText = users.age;
+//   userStadEl.innerText = users.stad;
+
+}
+})
+// .catch((error) => console.error("Error loading JSON file", error));
