ab4179d868883538b7c5d4ad4ccf2547640d15ef
Ufuk <hibbayt@gmail.com>
2024-02-08 14:22:58 +0100

    Code verkort en semantisch verbeterd

--- docs/script.js
+++ docs/script.js
@@ -1,28 +1,20 @@
-const usersNameEl = document.getElementById('naam');
-const userAgeEl = document.getElementById('age');
-const userStadEl = document.getElementById('stad')
-
 // Data fetchen
 fetch("/data/data.json")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    // console.log(data);
 
-      // Data showcasen
+// Data showcasen
   for (let users of data) {
-    // console.log(users);
-    const userNaam = document.createElement("h2");
-    const userAge = document.createElement('h2');
-    const userStad = document.createElement('h2');
-    userNaam.innerText = users.name;
-    userAge.innerText = users.age;
-    userStad.innerText = users.stad;
+    const usersNameEl = document.getElementById('naam');
+    const userAgeEl = document.getElementById('age');
+    const userStadEl = document.getElementById('stad');
 
-    usersNameEl.append(userNaam);
-    userAgeEl.append(userAge);
-    userStadEl.append(userStad);
+    usersNameEl.innerText = users.name;
+    userAgeEl.innerText = users.age;
+    userStadEl.innerText = users.stad;
+
   }
-
   })
   .catch((error) => console.error("Error loading JSON file", error));
   
