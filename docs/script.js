74632f1994d1dc4774465c40b8d6cf6d952e2295
Ufuk <hibbayt@gmail.com>
2024-02-08 20:31:07 +0100

    Tools section logic complete

--- docs/script.js
+++ docs/script.js
@@ -5,6 +5,7 @@ fetch("/data/data.json")
     // console.log(data);
 
 // Data showcasen
+// voor elke userS van data
   for (let users of data) {
     const usersNameEl = document.getElementById('naam');
     const userAgeEl = document.getElementById('age');
@@ -25,26 +26,39 @@ fetch("/data/skills.json")
   console.log(data);
 
 // Data showcasen
+  // voor elke tool van de fetch data (loop)
 for (let tool of data) {
+    // pak de container met de class .skillcontainer
     const skillscontainer = document.querySelector(".skillscontainer")
-    console.log(tool);
-    
-//     // nieuwe div aanmaken VOOR elke skill
-    // const skillNameEl = document.createElement('p');
-    // const skillImgEl = document.createElement('img');
+    // maak een article aan
+    const skillbox = document.createElement('article');
+    // voeg de class skillbox toe
+    skillbox.classList.add('skillbox')
 
+    // maak voor de skill naam een p aan
+    const skillName = document.createElement("p");
+    // vul de p met de textcontent uit de JSON
+    skillName.textContent = tool.skill;
+    // console.log(tool.skill);
 
+    // element creëeren voor image
+    const skillImg = document.createElement("img");
+    // class toevoegen aan image
+    skillImg.classList.add("skillimg");
+    // img source & alt text zetten
+    skillImg.src = tool.img;
+    skillImg.alt = tool.skill;
 
-    // skillNameEl.innerText = tool.skill;
-
-//   const usersNameEl = document.getElementById('naam');
-//   const userAgeEl = document.getElementById('age');
-//   const userStadEl = document.getElementById('stad');
-
-//   usersNameEl.innerText = users.name;
-//   userAgeEl.innerText = users.age;
-//   userStadEl.innerText = users.stad;
-
+    // zet skillImg en skillName in skillbox
+    skillbox.appendChild(skillImg);
+    skillbox.appendChild(skillName);
+    // zet skillbox in skillscontainer
+    skillscontainer.appendChild(skillbox);
 }
 })
-// .catch((error) => console.error("Error loading JSON file", error));
+.catch((error) => console.error("Error loading JSON file", error));
+
+// TUTORIAL voor de logica: https://www.youtube.com/watch?v=f4D50VnO_Gw&t=609s
+
+// RIJKSMUSEUM
+const rijksmuseumAPI = "https://www.rijkmuseum.nl/api/public/collection/items?key=12345&format=json";
